e7844005abfd6c3736900db958cd0f3bdd2faf4e
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-15 11:03:34 +0530

    feat:create a simple linked list

---
+++ LinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class Node
+    {
+        //Instance 
+        public int data;
+        public Node next;
+
+        //Constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Welcome Message
+            Console.WriteLine("Welcome To Linked List !");
+
+            //Calling Class LinkedList
+            LinkedList linkedList = new LinkedList();
+            linkedList.Add(56);
+            linkedList.Add(30);
+            linkedList.Add(70);
+        }
+    }
+}
